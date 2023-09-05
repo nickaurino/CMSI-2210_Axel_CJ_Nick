e5e1b3aba494e19c9375da06b7f709c98eb5c3e8
apestoniLMU <123414403+apestoniLMU@users.noreply.github.com>
2023-09-04 21:39:22 -0700

    sayhello.c (classwork)

--- Classwork/classwork01/sayhello.c
+++ Classwork/classwork01/sayhello.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main( int argc, char * argv[] ) {
-
-    printf( "\n\n   Hello, world! \n\n" );
+int main(int argc, char * argv[]) {
+    double degreesF;
+    char input[25];
+    printf("Enter a temp in degrees Fahrnenheit: ");
+    degreesF = atof(gets(input));
+    printf("%10.3f degrees F is %10.3f degrees C.", degreesF, (((degreesF-32.0)*5)/9));
     exit(0);
 }
