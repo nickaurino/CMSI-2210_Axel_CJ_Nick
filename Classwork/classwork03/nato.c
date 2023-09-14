cb0736e8551a66c3600481496a1d72a0b099face
nickaurino <nickaurino@gmail.com>
2023-09-13 17:17:15 -0700

    Create nato.c
    start letter to nato

---
+++ Classwork/classwork03/nato.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_VALUE 5999
+
+int main( int argc, char * argv[]){
+    char outputString[25] = "\0";
+    int index = 0;
+    int value = 0;
+    char * letter[] = {"a", "b", "c"};
+    char * letter[] = {"alpha", "beta", "charlie"};
+}
