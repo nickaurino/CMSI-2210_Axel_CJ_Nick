f8ae059715eb5892897a00bbec2fa4ea76ceb2c2
apestoniLMU <123414403+apestoniLMU@users.noreply.github.com>
2023-11-29 13:32:00 -0800

    Added Asserts

--- homework/homework06/gcdFinder.c
+++ homework/homework06/gcdFinder.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <assert.h>
-
+//nasm -f win32 findGCDfunc.nasm -o findGCDfunc.obj
+//gcc gcdFinder.c findGCDfunc.obj -m32 -o gcdFinder.exe
 extern int calculateGCD(int, int);
 
 int main() {
-    printf("FIND GCD %d", calulate_gcd(12,24));
-    return 0;
+    assert(calculateGCD(12,24)==12);
+    assert(calculateGCD(24,12)==12);
+    assert(calculateGCD(6,1)==1);
+    assert(calculateGCD(24,60)==12);
+    assert(calculateGCD(3,123)==3);
+    printf("WORKED");
 }
