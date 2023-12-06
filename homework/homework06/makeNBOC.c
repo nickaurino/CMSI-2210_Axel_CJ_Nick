4673f73e34efd4bf233465fd8ee7189c2cbd3a17
apestoniLMU <123414403+apestoniLMU@users.noreply.github.com>
2023-12-06 12:41:30 -0800

    Update makeNBOC.c

--- homework/homework06/makeNBOC.c
+++ homework/homework06/makeNBOC.c
@@ -2,7 +2,7 @@
 #include <stdint.h>
 #include <winsock.h>
 #include "NBOC.h"
-
+#gcc makeNBOC.c -o gcdFinder.exe
 long int makeNBOC(long int num) {
     if (num != htonl(num)) {
         num = htonl(num);
